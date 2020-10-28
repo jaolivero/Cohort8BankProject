26922898a34f1be912757fd3f6ec4595f8e31323
Joel Olivero <joelolivero@outlook.com>
2020-10-27 23:48:00 -0400

    worked on bank class

--- src/com/Bank/Bank.java
+++ src/com/Bank/Bank.java
@@ -1,10 +1,34 @@
 package com.Bank;
 
+import Account;
+import CheckingAccount;
+import SavingsAccount;
+
+import java.util.HashMap;
+
 public class Bank {
+    private String name;
+    private int safeAmt;
+    private HashMap<String, Client> clients;
+    private HashMap<Integer, Account> accounts;
+    private Integer clientCount = 0;
+    private Integer accountCount = 0;
 
+    public Bank(String name) {
+        this.name = name;
+        clients= new HashMap<>();
+        accounts = new HashMap<>();
+    }
 
-    // create a Bank class
-// 2 properties of your own.
-// listArray of Clients
-// method to add a client
+    public void addAccount() {
+        // TODO: get client
+        // TODO: create account giving it the client.
+        Account newAccount = new Account();
+    }
+
+    public Account createAccount() {}
+
+    public SavingsAccount creatsSavingsAccount() {};
+    public CheckingAccount creactCheckingAccount() {};
 }
+
--- src/com/Bank/Client.java
+++ src/com/Bank/Client.java
@@ -7,11 +7,11 @@ public class Client {
     private String firstName;
     private String lastName;
     private String clientId;
+    private Account account;
     private HashMap<Integer, Account> accounts;
     private List<DebitCard> cards;
 
     public Client(String name, List<BankAccount> accounts) {
-        this.name = firstName + " " + lastName;
         this.firstName = firstName;
         this.lastName = lastName;
         this.clientId = clientId;
@@ -20,8 +20,33 @@ public class Client {
     }
 
     public void addAccount(Account account) {
-        accounts.put(account.getAccountNum(), account);
+        accounts.put(account.getAccountNumber(), account);
     }
+
+    public void addDebitCard(int accountNum) {
+        Account account = accounts.get(accountNum);
+        if (account instanceof CheckingAccount) {
+            DebitCard newCard = new DebitCard("12341234", "1234", "987", this.name, (CheckingAccount) account);
+            debitCards.add(newCard);
+        } else {
+            System.out.println("Invalid account");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        for (int accountNum : accounts.keySet()) {
+            output += accounts.get(accountNum).getDetails() + "\n";
+        }
+        String cardsOutput = "";
+        for (var debitCard : debitCards) {
+            cardsOutput += debitCard.toString();
+        }
+        return "Accounts of " + name + "\n" + output + "Cards\n" + cardsOutput;
+    }
+
+}
 
 
 
